27bdffd91758f0df07df97b30a9a87bde7ef88cc
evelaveta <iluhina794@gmail.com>
2024-10-27 20:56:18 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python selection_sort.py && python merge_sort.py && python insertion_sort.py && python bubble_sort.py && python color_sort.py
+python selection_sort.py && python merge_sort.py && python insertion_sort.py && python bubble_sort.py && python timsort_sort.py
 
