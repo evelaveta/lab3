dcca35dedd722e1f49eb3eed6d862a12ad1cd4d2
evelaveta <iluhina794@gmail.com>
2024-10-28 00:14:39 +0300

    Update insertion_sort.py
    теперь точно работает

--- insertion_sort.py
+++ insertion_sort.py
@@ -14,25 +14,22 @@ SWAP_DELAY = 0
 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Color sort")
+pygame.display.set_caption("Insertion color sort")
 clock = pygame.time.Clock()
 
 
-def insertion_sort(arr, colors):
+def insertion_sort(arr):
     n = len(arr)
     for i in range(1, n):
         key = arr[i]
-        key_color = colors[i]
         j = i - 1
 
-        while j >= 0 and colors[j] > key_color:
+        while j >= 0 and arr[j] > key:
             arr[j + 1] = arr[j]
-            colors[j + 1] = colors[j]
             yield j + 1
             j -= 1
 
         arr[j + 1] = key
-        colors[j + 1] = key_color
         yield j + 1
 
 
@@ -44,19 +41,21 @@ def get_color(value, max_value):
     return (r, g, b)
 
 
-def draw_bars(arr, colors):
+
+def draw_bars(arr):
     screen.fill(BACKGROUND_COLOR)
     bar_width = WIDTH // len(arr)
-    bar_height = HEIGHT
 
     for i in range(len(arr)):
         color = get_color(arr[i], max_value)
+        bar_height = HEIGHT
+
         pygame.draw.rect(screen,
                          color,
-                         (i * bar_width, HEIGHT - bar_height, bar_width - 1, bar_height))
+                         (i * bar_width, HEIGHT - bar_height - 10, bar_width - 1, bar_height))
 
     pygame.display.flip()
-
+    
 def save_frame(frame_number):
     pygame.image.save(screen, f"frame_{frame_number:04d}.png")
 
@@ -65,9 +64,9 @@ def main():
     global max_value
     array_size = 150
     array = [random.randint(1, 100) for _ in range(array_size)]
-    colors = [get_color(value) for value in array]
 
-    sorting_generator = insertion_sort(array, colors)
+    max_value = max(array)
+    sorting_generator = insertion_sort(array)
 
     start_time = time.time()
     running = True
@@ -80,7 +79,7 @@ def main():
 
         try:
             indices_to_highlight = next(sorting_generator)
-            draw_bars(array, colors)
+            draw_bars(array)
 
             save_frame(frame_number)
             frames.append(f"frame_{frame_number:04d}.png")
@@ -90,7 +89,7 @@ def main():
 
             clock.tick(FPS)
         except StopIteration:
-            draw_bars(array, colors)
+            draw_bars(array)
 
             save_frame(frame_number)
             frames.append(f"frame_{frame_number:04d}.png")
